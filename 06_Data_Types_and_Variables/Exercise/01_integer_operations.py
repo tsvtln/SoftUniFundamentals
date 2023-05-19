29b5456f42e83132f7fb19e993bfd9b96dae185f
TSVTLN <tsvtln@abv.bg>
2023-05-19 12:01:57 +0300

    correcting right task

---
+++ 06_Data_Types_and_Variables/Exercise/01_integer_operations.py
@@ -0,0 +1,6 @@
+num1, num2, num3, num4 = int(input()), int(input()), int(input()), int(input())
+num2 += num1
+num2 //= num3
+num2 *= num4
+
+print(num2)
--- 06_Data_Types_and_Variables/Exercise/10_gladiator_expenses.py
+++
@@ -1,6 +0,0 @@
-num1, num2, num3, num4 = int(input()), int(input()), int(input()), int(input())
-num2 += num1
-num2 //= num3
-num2 *= num4
-
-print(num2)
