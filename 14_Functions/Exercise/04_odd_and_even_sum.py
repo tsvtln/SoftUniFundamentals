ae007993380ffcc24beedd84fe11f2d02aa1688b
TSVTLN <tsvtln@abv.bg>
2023-06-04 17:30:19 +0300

    Update 04_odd_and_even_sum.py

---
+++ 14_Functions/Exercise/04_odd_and_even_sum.py
@@ -0,0 +1,26 @@
+def is_even(number):
+    if int(number) % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def count_odd_even(list_of_numbers):
+    odd_numbers = 0
+    even_numbers = 0
+    for char in list_of_numbers:
+        if is_even(char):
+            even_numbers += int(char)
+        else:
+            odd_numbers += int(char)
+    return f'Odd sum = {odd_numbers}, Even sum = {even_numbers}'
+
+
+number_input = input()
+print(count_odd_even(number_input))
+
+
+
+
+
+
