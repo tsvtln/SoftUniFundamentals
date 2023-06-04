b5c4f3dff1e8aad9c09d5a434623cb50ac77ff82
TSVTLN <tsvtln@abv.bg>
2023-06-04 17:51:14 +0300

    Update 05_even_numbers.py

--- 14_Functions/Exercise/05_even_numbers.py
+++ 14_Functions/Exercise/05_even_numbers.py
@@ -1,17 +1,22 @@
-def is_even(number):
-    if int(number) % 2 == 0:
-        return True
-    else:
-        return False
+# def is_even(number):
+#     if int(number) % 2 == 0:
+#         return True
+#     else:
+#         return False
+#
+#
+# def deploy_even(list_of_numbers):
+#     even_numbers = []
+#     for number in list_of_numbers:
+#         if is_even(number):
+#             even_numbers.append(number)
+#     return even_numbers
+#
+#
+# receive_numbers = input().split(' ')
+# print(str(deploy_even(receive_numbers)).replace("'", ''))
+#
+#
 
 
-def deploy_even(list_of_numbers):
-    even_numbers = []
-    for number in list_of_numbers:
-        if is_even(number):
-            even_numbers.append(number)
-    return even_numbers
-
-
-receive_numbers = input().split(' ')
-print(deploy_even(receive_numbers))
+print([int(number) for number in input().split() if int(number) % 2 == 0])
