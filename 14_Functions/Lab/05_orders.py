29d48f2a92e899af171aa6a1ca75de973b28210e
TSVTLN <tsvtln@abv.bg>
2023-06-04 15:30:55 +0300

    Update 05_orders.py

--- 14_Functions/Lab/05_orders.py
+++ 14_Functions/Lab/05_orders.py
@@ -1,16 +1,14 @@
 def orders(product: str, quantity: int) -> float:
+    price = 0
     if product == 'water':
         price = quantity * 1
-        return price
     elif product == 'coffee':
         price = quantity * 1.5
-        return price
     elif product == 'coke':
         price = quantity * 1.4
-        return price
     elif product == 'snacks':
         price = quantity * 2
-        return price
+    return price
 
 
 inp_product = input()
