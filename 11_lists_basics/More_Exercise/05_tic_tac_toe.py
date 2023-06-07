cabf4a16a4bb97b4146eb571fe10295c0d996b06
TSVTLN <tsvtln@abv.bg>
2023-06-07 10:29:11 +0300

    Update 05_tic_tac_toe.py

--- 11_lists_basics/More_Exercise/05_tic_tac_toe.py
+++ 11_lists_basics/More_Exercise/05_tic_tac_toe.py
@@ -1,4 +1,47 @@
-line_one = input().split()
-line_two = input().split()
-line_three = input().split()
+line_one = [int(line_one) for line_one in input().split()]
+line_two = [int(line_two) for line_two in input().split()]
+line_three = [int(line_two) for line_two in input().split()]
+FP_WIN, SP_WIN = False, False
 
+# Check first player win conditions
+if line_one[0] == 1 and line_two[1] == 1 and line_three[2] == 1:
+    FP_WIN = True
+elif line_one[2] == 1 and line_two[1] == 1 and line_three[0] == 1:
+    FP_WIN = True
+elif line_one[0] == 1 and line_two[0] == 1 and line_three[0] == 1:
+    FP_WIN = True
+elif line_one[1] == 1 and line_two[1] == 1 and line_three[1] == 1:
+    FP_WIN = True
+elif line_one[2] == 1 and line_two[2] == 1 and line_three[2] == 1:
+    FP_WIN = True
+elif line_one[0] == 1 and line_one[0] == 1 and line_one[0] == 1:
+    FP_WIN = True
+elif line_two[0] == 1 and line_two[1] == 1 and line_two[2] == 1:
+    FP_WIN = True
+elif line_three[0] == 1 and line_three[1] == 1 and line_three[2] == 1:
+    FP_WIN = True
+
+# Check second player win conditions
+if line_one[0] == 2 and line_two[1] == 2 and line_three[2] == 2:
+    SP_WIN = True
+elif line_one[2] == 2 and line_two[1] == 2 and line_three[0] == 2:
+    SP_WIN = True
+elif line_one[0] == 2 and line_two[0] == 2 and line_three[0] == 2:
+    SP_WIN = True
+elif line_one[1] == 2 and line_two[1] == 2 and line_three[1] == 2:
+    SP_WIN = True
+elif line_one[2] == 2 and line_two[2] == 2 and line_three[2] == 2:
+    SP_WIN = True
+elif line_one[0] == 2 and line_one[0] == 2 and line_one[0] == 2:
+    SP_WIN = True
+elif line_two[0] == 2 and line_two[1] == 2 and line_two[2] == 2:
+    SP_WIN = True
+elif line_three[0] == 2 and line_three[1] == 2 and line_three[2] == 2:
+    SP_WIN = True
+
+if FP_WIN:
+    print("First player won")
+elif SP_WIN:
+    print("Second player won")
+else:
+    print("Draw!")
