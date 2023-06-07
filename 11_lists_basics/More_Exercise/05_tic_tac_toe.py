cd7b3a7f4163594a00e82f802ac0badc199ef631
TSVTLN <tsvtln@abv.bg>
2023-06-07 10:36:27 +0300

    Update 05_tic_tac_toe.py

--- 11_lists_basics/More_Exercise/05_tic_tac_toe.py
+++ 11_lists_basics/More_Exercise/05_tic_tac_toe.py
@@ -4,39 +4,25 @@ line_three = [int(line_two) for line_two in input().split()]
 FP_WIN, SP_WIN = False, False
 
 # Check first player win conditions
-if line_one[0] == 1 and line_two[1] == 1 and line_three[2] == 1:
-    FP_WIN = True
-elif line_one[2] == 1 and line_two[1] == 1 and line_three[0] == 1:
-    FP_WIN = True
-elif line_one[0] == 1 and line_two[0] == 1 and line_three[0] == 1:
-    FP_WIN = True
-elif line_one[1] == 1 and line_two[1] == 1 and line_three[1] == 1:
-    FP_WIN = True
-elif line_one[2] == 1 and line_two[2] == 1 and line_three[2] == 1:
-    FP_WIN = True
-elif line_one[0] == 1 and line_one[0] == 1 and line_one[0] == 1:
-    FP_WIN = True
-elif line_two[0] == 1 and line_two[1] == 1 and line_two[2] == 1:
-    FP_WIN = True
-elif line_three[0] == 1 and line_three[1] == 1 and line_three[2] == 1:
+if line_one[0] == 1 and line_two[1] == 1 and line_three[2] == 1 or \
+        line_one[2] == 1 and line_two[1] == 1 and line_three[0] == 1 or \
+        line_one[0] == 1 and line_two[0] == 1 and line_three[0] == 1 or \
+        line_one[1] == 1 and line_two[1] == 1 and line_three[1] == 1 or \
+        line_one[2] == 1 and line_two[2] == 1 and line_three[2] == 1 or \
+        line_one[0] == 1 and line_one[0] == 1 and line_one[0] == 1 or \
+        line_two[0] == 1 and line_two[1] == 1 and line_two[2] == 1 or \
+        line_three[0] == 1 and line_three[1] == 1 and line_three[2] == 1:
     FP_WIN = True
 
 # Check second player win conditions
-if line_one[0] == 2 and line_two[1] == 2 and line_three[2] == 2:
-    SP_WIN = True
-elif line_one[2] == 2 and line_two[1] == 2 and line_three[0] == 2:
-    SP_WIN = True
-elif line_one[0] == 2 and line_two[0] == 2 and line_three[0] == 2:
-    SP_WIN = True
-elif line_one[1] == 2 and line_two[1] == 2 and line_three[1] == 2:
-    SP_WIN = True
-elif line_one[2] == 2 and line_two[2] == 2 and line_three[2] == 2:
-    SP_WIN = True
-elif line_one[0] == 2 and line_one[0] == 2 and line_one[0] == 2:
-    SP_WIN = True
-elif line_two[0] == 2 and line_two[1] == 2 and line_two[2] == 2:
-    SP_WIN = True
-elif line_three[0] == 2 and line_three[1] == 2 and line_three[2] == 2:
+if line_one[0] == 2 and line_two[1] == 2 and line_three[2] == 2 or \
+        line_one[2] == 2 and line_two[1] == 2 and line_three[0] == 2 or \
+        line_one[0] == 2 and line_two[0] == 2 and line_three[0] == 2 or \
+        line_one[1] == 2 and line_two[1] == 2 and line_three[1] == 2 or \
+        line_one[2] == 2 and line_two[2] == 2 and line_three[2] == 2 or \
+        line_one[0] == 2 and line_one[0] == 2 and line_one[0] == 2 or \
+        line_two[0] == 2 and line_two[1] == 2 and line_two[2] == 2 or \
+        line_three[0] == 2 and line_three[1] == 2 and line_three[2] == 2:
     SP_WIN = True
 
 if FP_WIN:
