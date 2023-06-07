71c1eaefcf26eb210e7e0b3c460d6b690fe0de24
TSVTLN <tsvtln@abv.bg>
2023-06-07 22:44:44 +0300

    Update 05_orders.py

--- 14_Functions/Lab/05_orders.py
+++ 14_Functions/Lab/05_orders.py
@@ -1,17 +1,33 @@
 def orders(product: str, quantity: int) -> float:
-    price = 0
-    if product == 'water':
-        price = quantity * 1
-    elif product == 'coffee':
-        price = quantity * 1.5
-    elif product == 'coke':
-        price = quantity * 1.4
-    elif product == 'snacks':
-        price = quantity * 2
-    return price
+    prices = {'water': 1, 'coffee': 1.5, 'coke': 1.4, 'snacks': 2}
+
+    if product in prices:
+        price = quantity * prices[product]
 
 
-inp_product = input()
-inp_quantity = int(input())
-print(f'{orders(inp_product, inp_quantity):.2f}')
+try:
+    inp_product = input()
+    inp_quantity = int(input())
 
+    total_price = orders(inp_product, inp_quantity)
+    print(f'{total_price:.2f}')
+except ValueError as e:
+    print(str(e))
+
+# def orders(product: str, quantity: int) -> float:
+#     price = 0
+#     if product == 'water':
+#         price = quantity * 1
+#     elif product == 'coffee':
+#         price = quantity * 1.5
+#     elif product == 'coke':
+#         price = quantity * 1.4
+#     elif product == 'snacks':
+#         price = quantity * 2
+#     return price
+#
+#
+# inp_product = input()
+# inp_quantity = int(input())
+# print(f'{orders(inp_product, inp_quantity):.2f}')
+#
