3ac0d60dbb15168ee738eedfb03eb9a1ef79a7da
TSVTLN <tsvtln@abv.bg>
2023-06-08 11:55:07 +0300

    Update 01_data_types.py

---
+++ 14_Functions/More Exercise/01_data_types.py
@@ -0,0 +1,20 @@
+def data_types(command, value):
+    integer = 0
+    real = 0.00
+    string = ''
+    if command == 'int':
+        integer = value * 2
+        print(integer)
+    elif command == 'real':
+        real = float(value) * 1.5
+        print(f"{real:.2f}")
+    elif command == 'string':  # string
+        print(f"${value}$")
+
+
+command_entry, value_entry = input(), input()
+if value_entry.isnumeric():
+    value_entry = int(value_entry)
+
+data_types(command_entry, value_entry)
+# print(type(data_types(command_entry, value_entry)))
