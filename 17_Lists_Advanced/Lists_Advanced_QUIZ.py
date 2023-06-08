e345e674fc88ae4a57a9658461b683576d4a4655
TSVTLN <tsvtln@abv.bg>
2023-06-08 17:48:49 +0300

    Update Lists_Advanced_QUIZ.py

--- 17_Lists_Advanced/Lists_Advanced_QUIZ.py
+++ 17_Lists_Advanced/Lists_Advanced_QUIZ.py
@@ -103,6 +103,12 @@ Back = AnsiBack()
 Style = AnsiStyle()
 Cursor = AnsiCursor()
 
+# import time
+# minutes = 0
+# while minutes != 15:
+#     time.sleep(60)
+#     minutes += 1
+
 print(Fore.WHITE + 'Lists Advanced\nQUIZ by Mario Zahariev zahariev-webbersof@github')
 a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 = '', '', '', '', '', '', '', '', '', ''
 a1g, a2g, a3g, a4g, a5g, a6g, a7g, a8g, a9g, a10g = False, False, False, False, False, False, False, False, False, False
@@ -111,11 +117,11 @@ print(Fore.RED + "Provide the answers by typing 'a', 'b', 'c' or 'd'")
 print()
 
 # Question 1
-print(Fore.BLUE + '1. Which keyword is used to define a function in Python?')
-print(Fore.BLUE + 'a) method')
-print(Fore.BLUE + 'b) def ')
-print(Fore.BLUE + 'c) function')
-print(Fore.BLUE + 'd) define\n')
+print(Fore.BLUE + '1. What is the purpose of a lambda function in Python?')
+print(Fore.BLUE + 'a) To define a named function')
+print(Fore.BLUE + 'b) To create anonymous functions ')
+print(Fore.BLUE + 'c) To iterate over a list')
+print(Fore.BLUE + 'd) To swap elements in a list\n')
 
 while not a1g:
     a1 = input('Your answer: ').lower()
@@ -127,11 +133,11 @@ while not a1g:
 print("\n\n\n")
 
 # Question 2
-print(Fore.BLUE + '2. What is the purpose of the lambda keyword in Python?')
-print(Fore.BLUE + 'a) To define a class')
-print(Fore.BLUE + 'b) To create a variable')
-print(Fore.BLUE + 'c) To declare a function')
-print(Fore.BLUE + 'd) To define an anonymous function\n')
+print(Fore.BLUE + '2. Which of the following is a correct syntax for list comprehension in Python?')
+print(Fore.BLUE + 'a) [x for x in range(10)]')
+print(Fore.BLUE + 'b) [for x in range(10) if x % 2 == 0]')
+print(Fore.BLUE + 'c) [x if x % 2 == 0 for x in range(10)] ')
+print(Fore.BLUE + 'd) [x in range(10) if x % 2 == 0]\n')
 while not a2g:
     a2 = input('Your answer: ').lower()
     if a2 == 'a' or a2 == 'b' or a2 == 'c' or a2 == 'd':
@@ -143,11 +149,11 @@ print("\n\n\n")
 
 
 # Question 3
-print(Fore.BLUE + '3. What does the len() function return when applied to a list in Python?')
-print(Fore.BLUE + 'a) The sum of all elements in the list')
-print(Fore.BLUE + 'b) The number of items in the list ')
-print(Fore.BLUE + 'c) The average of all elements in the list')
-print(Fore.BLUE + 'd) The maximum value in the list\n')
+print(Fore.BLUE + '3. How can you swap the elements of two lists in Python?')
+print(Fore.BLUE + 'a) Using the swap() method')
+print(Fore.BLUE + 'b) Using the zip() function')
+print(Fore.BLUE + 'c) Using list concatenation')
+print(Fore.BLUE + 'd) Using simultaneous assignment or a temporary variable\n')
 while not a3g:
     a3 = input('Your answer: ').lower()
     if a3 == 'a' or a3 == 'b' or a3 == 'c' or a3 == 'd':
@@ -159,15 +165,15 @@ print("\n\n\n")
 
 
 # Question 4
-print(Fore.BLUE + '4. How do you call a function named my_function in Python?')
+print(Fore.BLUE + '4. Which function is used to extract unique elements from a list in Python?')
 # print(Fore.LIGHTYELLOW_EX + 'my_list = [1, 2, 3]')
 # print(Fore.LIGHTYELLOW_EX + 'my_list.remove(2)')
 # print(Fore.LIGHTYELLOW_EX + 'print(my_list)')
 # print()
-print(Fore.BLUE + 'a) call my_function()')
-print(Fore.BLUE + 'b) my_function')
-print(Fore.BLUE + 'c) invoke my_function()')
-print(Fore.BLUE + 'd) my_function()\n')
+print(Fore.BLUE + 'a) unique()')
+print(Fore.BLUE + 'b) distinct()')
+print(Fore.BLUE + 'c) set()')
+print(Fore.BLUE + 'd) list()\n')
 while not a4g:
     a4 = input('Your answer: ').lower()
     if a4 == 'a' or a4 == 'b' or a4 == 'c' or a4 == 'd':
@@ -178,11 +184,11 @@ while not a4g:
 print("\n\n\n")
 
 # Question 5
-print(Fore.BLUE + '5. What is the return type of the range() function in Python?')
-print(Fore.BLUE + 'a) Integer')
-print(Fore.BLUE + 'b) List')
-print(Fore.BLUE + 'c) Tuple')
-print(Fore.BLUE + 'd) Range object\n')
+print(Fore.BLUE + '5. What does the sorted() function do in Python?')
+print(Fore.BLUE + 'a) Sorts the elements of a list in ascending order ')
+print(Fore.BLUE + 'b) Sorts the elements of a list in descending order')
+print(Fore.BLUE + 'c) Removes duplicates from a list')
+print(Fore.BLUE + 'd) Reverses the order of elements in a list\n')
 print()
 while not a5g:
     a5 = input('Your answer: ').lower()
@@ -195,11 +201,11 @@ print("\n\n\n")
 
 
 # Question 6
-print(Fore.BLUE + '6. What does the map() function do in Python?')
-print(Fore.BLUE + 'a) Applies a function to each element of an iterable ')
-print(Fore.BLUE + 'b) Filters out elements from an iterable')
-print(Fore.BLUE + 'c) Computes the sum of all elements in an iterable')
-print(Fore.BLUE + 'd) Sorts the elements of an iterable\n')
+print(Fore.BLUE + '6. What is the purpose of the map() function in Python?')
+print(Fore.BLUE + 'a) To extract elements based on a condition')
+print(Fore.BLUE + 'b) To transform elements of a list based on a function ')
+print(Fore.BLUE + 'c) To sort elements in a list')
+print(Fore.BLUE + 'd) To extract unique elements from a list\n')
 while not a6g:
     a6 = input('Your answer: ').lower()
     if a6 == 'a' or a6 == 'b' or a6 == 'c' or a6 == 'd':
@@ -211,11 +217,11 @@ print("\n\n\n")
 
 
 # Question 7
-print(Fore.BLUE + '7. Can a function in Python have multiple return statements?')
-print(Fore.BLUE + 'a) Yes ')
-print(Fore.BLUE + 'b) No')
-print(Fore.BLUE + 'c) Only if the function has a single parameter')
-print(Fore.BLUE + 'd) Only if the function is defined using lambda\n')
+print(Fore.BLUE + '7. Which function is used to filter elements from a list based on a condition in Python?')
+print(Fore.BLUE + 'a) select()')
+print(Fore.BLUE + 'b) find()')
+print(Fore.BLUE + 'c) filter() ')
+print(Fore.BLUE + 'd) match()\n')
 while not a7g:
     a7 = input('Your answer: ').lower()
     if a7 == 'a' or a7 == 'b' or a7 == 'c' or a7 == 'd':
@@ -227,11 +233,11 @@ print("\n\n\n")
 
 
 # Question 8
-print(Fore.BLUE + '8. Can a function in Python have optional parameters?')
-print(Fore.BLUE + 'a) Yes ')
-print(Fore.BLUE + 'b) No')
-print(Fore.BLUE + 'c) Only if the function is defined using lambda')
-print(Fore.BLUE + 'd) Only if the function has a single line of code\n')
+print(Fore.BLUE + '8. How can you check if a number is divisible by 3 using a lambda function in Python?')
+print(Fore.BLUE + 'a) lambda x: x % 3 == 0 ')
+print(Fore.BLUE + 'b) lambda x: x / 3 == 0')
+print(Fore.BLUE + 'c) lambda x: x == 3')
+print(Fore.BLUE + 'd) lambda x: x % 3\n')
 while not a8g:
     a8 = input('Your answer: ').lower()
     if a8 == 'a' or a8 == 'b' or a8 == 'c' or a8 == 'd':
@@ -243,11 +249,15 @@ print("\n\n\n")
 
 
 # Question 9
-print(Fore.BLUE + '9. What is the scope of a variable defined inside a function in Python?')
-print(Fore.BLUE + 'a) Global scope')
-print(Fore.BLUE + 'b) Class scope')
-print(Fore.BLUE + 'c) Function scope ')
-print(Fore.BLUE + 'd) Module scope\n')
+print(Fore.BLUE + '9.What is the output of the following list comprehension?')
+print(Fore.LIGHTYELLOW_EX + 'numbers = [1, 2, 3, 4, 5]')
+print(Fore.LIGHTYELLOW_EX + 'squares = [x ** 2 for x in numbers]')
+print(Fore.LIGHTYELLOW_EX + 'print(squares)')
+print()
+print(Fore.BLUE + 'a) [1, 4, 9, 16, 25] ')
+print(Fore.BLUE + 'b) [2, 4, 6, 8, 10]')
+print(Fore.BLUE + 'c) [1, 3, 5, 7, 9]')
+print(Fore.BLUE + 'd) [1, 2, 3, 4, 5]\n')
 while not a9g:
     a9 = input('Your answer: ').lower()
     if a9 == 'a' or a9 == 'b' or a9 == 'c' or a9 == 'd':
@@ -259,12 +269,14 @@ print("\n\n\n")
 
 
 # Question 10
-print(Fore.BLUE + '10. What is a recursive function in Python?')
-
-print(Fore.BLUE + 'a) A function that calls itself ')
-print(Fore.BLUE + 'b) A function defined inside another function')
-print(Fore.BLUE + 'c) A function that accepts a variable number of arguments')
-print(Fore.BLUE + 'd) A function that returns a boolean value\n')
+print(Fore.BLUE + '10. What is the output of the following:')
+print(Fore.LIGHTYELLOW_EX + 'my_list = [1, 2, 3]')
+print(Fore.LIGHTYELLOW_EX + 'my_list.extend([4, 5])')
+print()
+print(Fore.BLUE + 'a) No output')
+print(Fore.BLUE + 'b) Extend is not a valid function')
+print(Fore.BLUE + 'c) [4, 5, 1, 2, 3]')
+print(Fore.BLUE + 'd) [1, 2, 3, 4, 5]\n')
 
 while not a10g:
     a10 = input('Your answer: ').lower()
