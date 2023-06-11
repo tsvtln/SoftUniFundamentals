4933413d86a7340656151b9b196a5744c6ca3bd0
TSVTLN <tsvtln@abv.bg>
2023-06-11 17:42:24 +0300

    Update 03_to_do_list.py

--- 17_Lists_Advanced/Lab/03_to_do_list.py
+++ 17_Lists_Advanced/Lab/03_to_do_list.py
@@ -8,7 +8,7 @@ while CONTINUE:
         continue
     command_list.append(command)
 
-command_list.sort()
+command_list = sorted(command_list, key=lambda x: int(x.split('-')[0]))
 for item in command_list:
     item_thing = item.split('-')
     to_do_list.append(item_thing[1])
