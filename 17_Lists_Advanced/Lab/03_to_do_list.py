5a86329e6ce9c41c2edc88b55558b65b23adfa97
TSVTLN <tsvtln@abv.bg>
2023-06-11 17:32:11 +0300

    Update 03_to_do_list.py

---
+++ 17_Lists_Advanced/Lab/03_to_do_list.py
@@ -0,0 +1,16 @@
+CONTINUE = True
+to_do_list = []
+command_list = []
+while CONTINUE:
+    command = input()
+    if command == 'End':
+        CONTINUE = False
+        continue
+    command_list.append(command)
+
+command_list.sort()
+for item in command_list:
+    item_thing = item.split('-')
+    to_do_list.append(item_thing[1])
+
+print(to_do_list)
