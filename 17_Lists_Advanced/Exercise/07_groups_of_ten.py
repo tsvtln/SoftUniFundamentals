fcff3024bce6727ddb5f8373109dff953da6607a
TSVTLN <tsvtln@abv.bg>
2023-06-22 14:26:24 +0300

    Update 07_groups_of_ten.py

---
+++ 17_Lists_Advanced/Exercise/07_groups_of_ten.py
@@ -0,0 +1,15 @@
+sequence = [int(x) for x in input().split(', ')]
+group_num = 10
+while len(sequence) > 0:
+    group_list = []
+    for number in sequence:
+        if number <= group_num:
+            group_list.append(number)
+            sequence
+    print(f"Group of {group_num}'s: {group_list}")
+    for number in group_list:
+        if number in sequence:
+            sequence.remove(number)
+    group_num += 10
+
+
