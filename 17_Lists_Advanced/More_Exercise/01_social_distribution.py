34a4f55f009343bd06a40796f8258a929d113206
TSVTLN <tsvtln@abv.bg>
2023-06-27 12:30:25 +0300

    Update 01_social_distribution.py

--- 17_Lists_Advanced/More_Exercise/01_social_distribution.py
+++ 17_Lists_Advanced/More_Exercise/01_social_distribution.py
@@ -33,3 +33,19 @@ def find_wealthiest(population: list):
 entry_population, entry_wealth = [int(x) for x in input().split(', ')], int(input())
 DSTRBTBL, pop_list = distribute_wealth(entry_population, entry_wealth)
 print(pop_list) if DSTRBTBL else print("No equal distribution possible")
+
+"""100/100"""
+# population = list(map(int, input().split(", ")))
+# minimum_wealth = int(input())
+# countries_count = len(population)
+# if sum(population) < countries_count * minimum_wealth:
+#     print("No equal distribution possible")
+# else:
+#     while min(population) < minimum_wealth:
+#         for index, country in enumerate(population):
+#             if country < minimum_wealth:
+#                 difference = minimum_wealth - country
+#                 population[index] += difference
+#                 max_population_index = population.index(max(population))
+#                 population[max_population_index] -= difference
+#     print(population)
