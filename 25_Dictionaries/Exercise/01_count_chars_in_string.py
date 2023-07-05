4400fdfac6aaa204b14010b037790bb3aae89ae9
TSVTLN <tsvtln@abv.bg>
2023-07-05 10:54:53 +0300

    Update 01_count_chars_in_string.py

--- 25_Dictionaries/Exercise/01_count_chars_in_string.py
+++ 25_Dictionaries/Exercise/01_count_chars_in_string.py
@@ -1,6 +1,4 @@
-text = input()
-letters = [char for char in text]
-count_chars = dict()
+letters, count_chars = [char for char in input()], dict()
 for letter in letters:
     if letter not in count_chars and letter != ' ':
         count_chars[letter] = 1
@@ -8,8 +6,6 @@ for letter in letters:
         count_chars[letter] += 1
 
 for letter, count in count_chars.items():
-    ltr = letter
-    cnt = count
-    print(f"{ltr} -> {cnt}")
+    print(f"{letter} -> {count}")
 
 
