4b7a5baf92e07da823d661f059028e0cc3bd9da6
TSVTLN <tsvtln@abv.bg>
2023-07-05 16:45:44 +0300

    Update 03_capitals.py

--- 25_Dictionaries/Exercise/03_capitals.py
+++ 25_Dictionaries/Exercise/03_capitals.py
@@ -1,3 +1,4 @@
 countries, capitals = input().split(', '), input().split(', ')
 output = {countries[index]: capitals[index] for index in range(len(capitals))}
+# output = dict(zip(countries, capitals))  # zip method to create a dictionary without comprehension
 printing = {print(f"{country} -> {capital}") for country, capital in output.items()}
