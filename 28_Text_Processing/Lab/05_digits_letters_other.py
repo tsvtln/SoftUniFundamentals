3afdab6de745d766b1c6f1ec1a6ded82e2c96c22
TSVTLN <tsvtln@abv.bg>
2023-07-24 12:50:21 +0300

    Update 05_digits_letters_other.py

---
+++ 28_Text_Processing/Lab/05_digits_letters_other.py
@@ -0,0 +1,13 @@
+string = input()
+digits = ''
+alpha = ''
+symbols = ''
+for ch in string:
+    if ch.isdigit():
+        digits += ch
+    elif ch.isalpha():
+        alpha += ch
+    else:
+        symbols += ch
+print(f"{digits}\n{alpha}\n{symbols}")
+        
