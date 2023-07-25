2163c8717c59474a418d0d4f2b550e4ab58319c9
TSVTLN <tsvtln@abv.bg>
2023-07-25 12:49:38 +0300

    Update 07_string_explosion.py

---
+++ 28_Text_Processing/Exercise/07_string_explosion.py
@@ -0,0 +1,13 @@
+string = input()
+final_text = ''
+explosion_power = 0
+EXPLOSION = False
+for char in string:
+    if char.isnumeric():
+        explosion_power += int(char)
+    if explosion_power != 0 and char != '>':
+        explosion_power -= 1
+        continue
+    else:
+        final_text += char
+print(final_text)
