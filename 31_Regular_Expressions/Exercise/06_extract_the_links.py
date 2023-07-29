535cc17a8fb6be618b5c8dd68f5fd95fbf4361b1
TSVTLN <tsvtln@abv.bg>
2023-07-29 19:52:25 +0300

    Update 06_extract_the_links.py

--- 31_Regular_Expressions/Exercise/06_extract_the_links.py
+++ 31_Regular_Expressions/Exercise/06_extract_the_links.py
@@ -2,14 +2,9 @@ import re
 
 pattern = r"(www\.([A-Za-z0-9\-]+)\.[a-z]+(\.[a-z]+)*)"
 
-
 string = input()
 while string:
     mails = re.search(pattern, string)
     if mails:
         print(mails.group(0))
-
     string = input()
-
-
-
