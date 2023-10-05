dd74d00b80685ab0277e3888d9d7b28650eb6bd5
TSVTLN <tsvtln@abv.bg>
2023-10-05 23:33:11 +0300

    Update 03_kates_way_out.py

--- 17_Lists_Advanced/More_Exercise/03_kates_way_out.py
+++ 17_Lists_Advanced/More_Exercise/03_kates_way_out.py
@@ -48,3 +48,46 @@ for m in range(rows_in_maze):
     line = input()
     maze_structure.append(line)
 print(fnd_kate(maze_structure))
+
+
+"""100/100"""
+# size = int(input())
+# maze = []
+# for _ in range(size):
+#     row = input()
+#     row = [x for x in row]
+#     maze.append(row)
+# kate_position = []
+# for row in range(len(maze)):
+#     for column in range(len(maze[row])):
+#         if maze[row][column] == "k":
+#             kate_position = [row, column]
+# row = kate_position[0]
+# column = kate_position[1]
+# escape_fond = False
+# while True:
+#     if row - 1 < 0 or column - 1 < 0 or row + 1 > len(maze) - 1 or column + 1 > len(maze[0]) - 1:
+#         escape_fond = True
+#         break
+#     if maze[row - 1][column] != " " and maze[row][column - 1] != " " and \
+#             maze[row + 1][column] != " " and maze[row][column + 1] != " ":
+#         print("Kate cannot get out")
+#         break
+#     if maze[row - 1][column] == " ":
+#         maze[row - 1][column] = "k"
+#         row -= 1
+#     elif maze[row][column - 1] == " ":
+#         maze[row][column - 1] = "k"
+#         column -= 1
+#     elif maze[row + 1][column] == " ":
+#         maze[row + 1][column] = "k"
+#         row += 1
+#     elif maze[row][column + 1] == " ":
+#         maze[row][column + 1] = "k"
+#         column += 1
+# if escape_fond:
+#     count_moves = 0
+#     for x in maze:
+#         count_moves += x.count("k")
+#         count_moves += x.count(" ")
+#     print(f'Kate got out in {count_moves} moves')
